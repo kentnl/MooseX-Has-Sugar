ee565dd0a7b6e7680730556e8a4a1867946507dd
Kent Fredric <kentfredric@gmail.com>
2009-12-04 20:56:37 +1300

    Reformat documentation of Minimal

--- lib/MooseX/Has/Sugar/Minimal.pm
+++ lib/MooseX/Has/Sugar/Minimal.pm
@@ -26,67 +26,70 @@ and C<rw> functions, the way L<MooseX::Has::Sugar> used to with C<:is>;
             lazy_build => 1,
     );
 
+All functions are exported by L<Sub::Exporter>.
+
+=cut
+
+=export_group :default
+
+Exports L</:is>
+
+=export_group :is
+
+Exports L</bare>, L</ro>, L</rw>
+
 =cut
 
 Sub::Exporter::setup_exporter(
-    {
-        exports => [ 'ro', 'rw', 'bare', ],
-        groups  => {
-            is      => [ 'ro', 'rw', 'bare', ],
-            default => ['-is'],
-        }
+  {
+    exports => [ 'ro', 'rw', 'bare', ],
+    groups  => {
+      is      => [ 'ro', 'rw', 'bare', ],
+      default => ['-is'],
     }
+  }
 );
 
+=export_function bare
+
+returns C<('bare')>
+
+=cut
+
 sub bare() {
-    return ('bare');
+  return ('bare');
 }
+
+=export_function ro
+
+returns C<('ro')>
+
+=cut
 
 sub ro() {
-    return ('ro');
+  return ('ro');
 }
 
+=export_function rw
+
+returns C<('rw')>
+
+=cut
+
 sub rw() {
-    return ('rw');
+  return ('rw');
 }
 
 1;
 
 __END__
 
-
-=head1 EXPORT
-
-=over 4
-
-=item rw
-
-=item ro
-
-=item bare
-
-=back
-
-=head1 EXPORT GROUPS
-
-=over 4
-
-=item :default
-
-Exports C<:is>
-
-=item :is
-
-Exports C<ro> and C<rw> and C<bare>
-
-=back
-
 =head1 CONFLICTS
 
 This module is not intended to be used in conjunction with
  L<MooseX::Has::Sugar>.
 
-We both export C<ro> and C<rw> in different ways.
+We both export L</ro> and L</rw> in different ways.
 
 If you do however want to use them in conjuction, specific imports must
  be done on L<MooseX::Has::Sugar>'s side to stop it exporting different
@@ -101,28 +104,5 @@ If you do however want to use them in conjuction, specific imports must
     use MooseX::Has::Sugar qw( lazy_build );
 
     has foo =>( is => rw , lazy_build );
-
-=head1 FUNCTIONS
-
-These you probably don't care about, they're all managed by L<Sub::Exporter>
- and its stuff anyway.
-
-=over 4
-
-=item rw
-
-returns C<('rw')>
-
-=item ro
-
-returns C<('ro')>
-
-=item bare
-
-returns C<('bare')>
-
-=back
-
-=head1 ACKNOWLEDGEMENTS
 
 =cut
