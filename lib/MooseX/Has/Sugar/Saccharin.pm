8b36924935b48c7f64305ebed96d91b9bd27f08a
Kent Fredric <kentfredric@gmail.com>
2014-01-31 06:05:36 +1300

    Build results of 23fe603 (on master)

--- lib/MooseX/Has/Sugar/Saccharin.pm
+++ lib/MooseX/Has/Sugar/Saccharin.pm
@@ -31,6 +31,12 @@ use Sub::Exporter ();
 
 
 
+
+
+
+
+
+
 Sub::Exporter::setup_exporter(
   {
     exports => [
@@ -310,8 +316,14 @@ Your choice.
 
 =head2 C<:default>
 
-exports  L</ro>, L</rw>, L</required>, L</lazy>, L</lazy_build>, L</coerce>, L</weak_ref>, L</auto_deref>,
-      L</bare>, L</default>, L</init_arg>, L</predicate>, L</clearer>, L</builder>, L</trigger>,
+exports:
+
+=over 4
+
+L</ro>, L</rw>, L</required>, L</lazy>, L</lazy_build>, L</coerce>, L</weak_ref>, L</auto_deref>
+L</bare>, L</default>, L</init_arg>, L</predicate>, L</clearer>, L</builder>, L</trigger>
+
+=back
 
 =head1 EXPORTED FUNCTIONS
 
