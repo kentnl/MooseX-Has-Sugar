a8805f162c3faf3b32830ce1442baef982214b9c
Kent Fredric <kentfredric@gmail.com>
2014-06-26 09:48:43 +1200

    Improve ::Sugar coverage

--- t/lib/T4Values/DEverything.pm
+++ t/lib/T4Values/DEverything.pm
@@ -13,10 +13,50 @@ sub generated {
   };
 }
 
+sub generated_bare {
+  {
+    isa => 'Str',
+    bare, required, lazy, lazy_build, coerce, weak_ref, auto_deref
+  };
+}
+
+sub generated_rw {
+  {
+    isa => 'Str',
+    rw, required, lazy, lazy_build, coerce, weak_ref, auto_deref
+  };
+}
+
 sub manual {
   {
     isa        => 'Str',
     is         => 'ro',
+    required   => 1,
+    lazy       => 1,
+    lazy_build => 1,
+    coerce     => 1,
+    weak_ref   => 1,
+    auto_deref => 1,
+  };
+}
+
+sub manual_rw {
+  {
+    isa        => 'Str',
+    is         => 'rw',
+    required   => 1,
+    lazy       => 1,
+    lazy_build => 1,
+    coerce     => 1,
+    weak_ref   => 1,
+    auto_deref => 1,
+  };
+}
+
+sub manual_bare {
+  {
+    isa        => 'Str',
+    is         => 'bare',
     required   => 1,
     lazy       => 1,
     lazy_build => 1,
