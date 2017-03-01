59052950677fcab6cd8edc22d2a2c0d2eacf1786
Kent Fredric <kentfredric@gmail.com>
2017-03-01 19:14:12 +1300

    Build results of 67cb89e (on master)

--- lib/MooseX/Has/Sugar/Minimal.pm
+++ lib/MooseX/Has/Sugar/Minimal.pm
@@ -4,7 +4,7 @@ use strict;
 
 package MooseX::Has::Sugar::Minimal;
 
-our $VERSION = '1.000005';
+our $VERSION = '1.000006';
 
 # ABSTRACT: Less Sugary Syntax for moose 'has' fields
 
@@ -72,7 +72,7 @@ MooseX::Has::Sugar::Minimal - Less Sugary Syntax for moose 'has' fields
 
 =head1 VERSION
 
-version 1.000005
+version 1.000006
 
 =head1 SYNOPSIS
 
